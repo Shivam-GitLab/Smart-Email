1811ed44ded3018bbf2c71066461a1ea0af27dc4
Shivam-GitLab <shivamkumarsaitm@gmail.com>
2025-08-07 23:22:38 +0530

    Key Update

--- src/main/java/com/sm/smartEmail/SmartEmailApplication.java
+++ src/main/java/com/sm/smartEmail/SmartEmailApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartEmailApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SmartEmailApplication.class, args);
     }
-
 }
